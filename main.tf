e1ac3b8f189986718c9b32af319e965b5ef6714d
PR <radar.fami@gmail.com>
2023-02-16 22:27:29 +0100

    services

--- main.tf
+++ main.tf
@@ -5,9 +5,19 @@ resource "random_id" "id" {
 resource "google_project" "project" {
   name = var.name
   project_id = format(
-    "%s-%s", 
-    lower(var.name), 
+    "%s-%s",
+    lower(var.name),
     random_id.id.dec
   )
   folder_id = var.folder_id
+}
+
+resource "google_project_service" "services" {
+  project                    = google_project.project.project_id
+  for_each                   = toset(var.services)
+  service                    = each.key
+  disable_dependent_services = true
+  depends_on = [
+    google_project.project
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -3,5 +3,9 @@ variable "name" {
 }
 
 variable "folder_id" {
- description = "Project's parent folder ID" 
+  description = "Project's parent folder ID"
+}
+
+variable "services" {
+  description = "List of APIs that should be enabled in the project"
 }
