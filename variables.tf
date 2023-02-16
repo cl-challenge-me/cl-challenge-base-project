8ff74df3cdf2015a1805c228fea3f98ce53d30cf
PR <radar.fami@gmail.com>
2023-02-16 22:30:53 +0100

    types

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "name" {
   description = "Project name"
+  type        = string
 }
 
 variable "folder_id" {
   description = "Project's parent folder ID"
+  type        = string
 }
 
 variable "services" {
   description = "List of APIs that should be enabled in the project"
+  type        = list(string)
 }
