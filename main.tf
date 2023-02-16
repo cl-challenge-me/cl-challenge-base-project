7e9f418809cef1c4577c44d246accc6690b57709
PR <radar.fami@gmail.com>
2023-02-16 22:16:48 +0100

    project_id and random id

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+resource "random_id" "id" {
+  byte_length = 2
+}
+
 resource "google_project" "project" {
   name = var.name
+  project_id = format(
+    "%s-%s", 
+    lower(var.name), 
+    random_id.id.dec
+  )
   folder_id = var.folder_id
 }
